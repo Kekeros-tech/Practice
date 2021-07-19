71362dd4775033a00abad0b30dbc947e55c322fa
Kekeros-tech <69265505+Kekeros-tech@users.noreply.github.com>
2021-07-19 22:34:54 +0300

    This is my first try to use git in IDEA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello world");
-	System.out.println("Here I wrote other line");// write your code here
+	System.out.println("Here I wrote other line");
+	System.out.println("well, it`s work");// write your code here
     }
 }
